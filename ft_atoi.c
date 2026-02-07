40513dd2329ffd2f0ab36a6e11c1f91dfdec7b8f
cherica <riantsoacherica01@gmail.com>
2026-02-07 19:54:23 +0300

    mamarana

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,24 +1,33 @@
-
 #include "libft.h"
 
-int
-	ft_atoi(const char *str)
+int	ft_atoi(const char *str)
 {
 	int	i;
-	int	is_neg;
-	int	res;
+	int	sign;
+	int	result;
 
-	if (!str)
-		return (0);
 	i = 0;
-	while (str[i] == '\t' || str[i] == '\n' || str[i] == '\v' ||
-			str[i] == '\f' || str[i] == '\r' || str[i] == ' ')
+	sign = 1;
+	result = 0;
+
+	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t'
+		|| str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
 		i++;
-	is_neg = (str[i] == '-') ? -1 : 1;
-	if (is_neg == -1 || str[i] == '+')
+
+	
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
 		i++;
-	res = 0;
+	}
+
+	
 	while (str[i] >= '0' && str[i] <= '9')
-		res = (res * 10) + (str[i++] - '0');
-	return (res * is_neg);
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+
+	return (result * sign);
 }
--- ft_strncmp.c
+++ ft_strncmp.c
@@ -1,35 +1,21 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strncmp.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ncolomer <ncolomer@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/10/07 14:22:36 by ncolomer          #+#    #+#             */
-/*   Updated: 2019/10/10 01:53:03 by ncolomer         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "libft.h"
 
-static int
-	cmp_char(char c1, char c2)
+static int	cmp_char(char c1, char c2)
 {
-	if ((unsigned char)c1 != (unsigned char)c2)
-		return ((unsigned char)c1 - (unsigned char)c2);
-	return (0);
+	return ((unsigned char)c1 - (unsigned char)c2);
 }
 
-int
-	ft_strncmp(const char *s1, const char *s2, size_t n)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t			i;
+	size_t	i;
 
 	i = 0;
-	while (s1[i] && s2[i] && i < n)
+	if (n == 0)
+		return (0);
+	while (i < n && s1[i] && s2[i])
 	{
-		if (cmp_char(s1[i], s2[i]))
-			return (s1[i] - s2[i]);
+		if (cmp_char(s1[i], s2[i]) != 0)
+			return (cmp_char(s1[i], s2[i]));
 		i++;
 	}
 	if (i < n)
