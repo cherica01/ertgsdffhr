df6ce17c70366129d08aa5bf6c2eb29e92a09cf8
cherica <riantsoacherica01@gmail.com>
2026-02-08 17:57:03 +0300

    split

---
+++ ft_split.c
@@ -0,0 +1,63 @@
+#include "libft.h"
+
+static size_t	count_words(char const *s, char c)
+{
+	size_t	i;
+	size_t	count;
+
+	i = 0;
+	count = 0;
+	while (s[i])
+	{
+		if (s[i] != c)
+		{
+			count++;
+			while (s[i] && s[i] != c)
+				i++;
+		}
+		else
+			i++;
+	}
+	return (count);
+}
+
+static char	*get_word(char const *s, char c, size_t *i)
+{
+	size_t	start;
+	size_t	len;
+
+	while (s[*i] == c)
+		(*i)++;
+	start = *i;
+	len = 0;
+	while (s[*i] && s[*i] != c)
+	{
+		len++;
+		(*i)++;
+	}
+	return (ft_substr(s, start, len));
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**tab;
+	size_t	i;
+	size_t	j;
+
+	if (!s)
+		return (NULL);
+	tab = malloc(sizeof(char *) * (count_words(s, c) + 1));
+	if (!tab)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (j < count_words(s, c))
+	{
+		tab[j] = get_word(s, c, &i);
+		if (!tab[j])
+			return (NULL);
+		j++;
+	}
+	tab[j] = NULL;
+	return (tab);
+}
