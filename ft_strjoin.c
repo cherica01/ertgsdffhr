20022789eb65c6e21d4ab0adf5dd783ebdee4ca8
cherica <riantsoacherica01@gmail.com>
2026-02-08 08:06:56 +0300

    ft_strjoin

---
+++ ft_strjoin.c
@@ -0,0 +1,37 @@
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	i;
+	size_t	j;
+	size_t	len1;
+	size_t	len2;
+	char	*str;
+
+	if (!s1 || !s2)
+		return (NULL);
+
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+
+	str = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
+	if (!str)
+		return (NULL);
+
+	i = 0;
+	while (i < len1)
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (j < len2)
+	{
+		str[i + j] = s2[j];
+		j++;
+	}
+	str[i + j] = '\0';
+	return (str);
+}
+
